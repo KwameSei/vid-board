7e4659ca62f815c1a0db64653e451b37a15fd03e
KwameSei <nathanielosei92@gmail.com>
2022-11-24 20:39:18 +0000

    Video panels created

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -3,8 +3,10 @@
 //Declaring variables and selecting class names with 'querySelector'
 var menuIcon = document.querySelector(".menu-icon");    
 var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container")
 
 //Changing width of the sidebar when menu icon is clicked
 menuIcon.onclick = function(){
     sidebar.classList.toggle("mini-sidebar")    //Mini sidebar is  another class name
+    container.classList.toggle("expand-container")
 }
